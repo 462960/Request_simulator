357708e140253932b0c3b7ae25f9e53bd46ca591
Volodymyr <462960@gmailcom>
2020-04-26 12:57:45 +0300

    Some animation added

--- src/components/LoadItem.js
+++ src/components/LoadItem.js
@@ -1,9 +1,10 @@
 import React from "react";
+import cn from "classnames";
 
 import Button from "@atlaskit/button";
 import BookIcon from "@atlaskit/icon/glyph/trash";
 
-const LoadItem = ({ loaders, removeLoader }) => {
+const LoadItem = ({ loaders, removeLoader, currentLoader, onHold }) => {
   const deleteLoader = (n) => {
     const i = loaders.findIndex((y) => y.id === n);
     removeLoader(loaders, i);
@@ -11,17 +12,21 @@ const LoadItem = ({ loaders, removeLoader }) => {
 
   return (
     <div>
-      {(loaders || []).map((x) => (
-        <ul key={x.id} className="table-unit">
-          <li>{x.name}</li>
-          <li>{x.delay}</li>
-          <li>
-            <Button onClick={() => deleteLoader(x.id)}>
-              <BookIcon />
-            </Button>
-          </li>
-        </ul>
-      ))}
+      {(loaders || []).map((x) => {
+        let blinking = currentLoader === x.name;
+        let paused = blinking && onHold;
+        return (
+          <ul key={x.id} className="table-unit">
+            <li className={cn({ blinking })}>{x.name}</li>
+            {paused ? <li className="paused">PAUSED</li> : <li>{x.delay}</li>}
+            <li>
+              <Button onClick={() => deleteLoader(x.id)}>
+                <BookIcon />
+              </Button>
+            </li>
+          </ul>
+        );
+      })}
     </div>
   );
 };
